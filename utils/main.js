22bc13245cd915634640214e7665ad82ce40538f
Alvin <alvinvaati@gmail.com>
2023-10-17 23:49:25 +0300

    .

--- utils/main.js
+++
@@ -1,6 +0,0 @@
-import dbClient from './db.js';
-import redisClient from './redis.js';
-
-console.log(dbClient.isAlive());
-// console.log(redisClient.isAlive());
-// console.log(redisClient.con());
