ac9a1bf91682597da5aee5605a41bd4d4034841f
Alvin <alvinvaati@gmail.com>
2023-10-17 23:49:19 +0300

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,16 @@
-const express = require('express');
+import express from 'express';
+import user from './user.js';
+import admin from './admin.js';
+import product from './product.js';
+
 const router = express.Router();
 
+// Welcome Page
+router.get('/', (req, res) => res.json('welcome'));
 
+//user
+router.use('/user', user);
+//admin
+router.use('/admin', admin);
+router.use('/product', product);
+export default router;
