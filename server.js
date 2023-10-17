50f3013808416e10dc705de9fd27aa2f5cad7b40
Alvin <alvinvaati@gmail.com>
2023-10-17 23:49:08 +0300

    server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import router from './routes/index.js';
+
+// Load environment variables
+dotenv.config();
+
+// Set up port
 const port = process.env.PORT || 5000;
-const express = require('express');
-const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +16,8 @@ app.use(cors());
 // Parse JSON
 app.use(express.json({limit:'200mb'}));
 
+// Routes
+app.use('/api/v1', router);
 // Start the server
 
 app.listen(port, () => {
