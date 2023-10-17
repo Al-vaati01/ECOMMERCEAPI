dc3c72105cdabb26404b8d470e20e6d447d8939d
Alvin <alvinvaati@gmail.com>
2023-10-17 22:36:04 +0300

    redis class

--- utils/redis.js
+++ utils/redis.js
@@ -25,6 +25,14 @@ class RedisClient {
   async del(key) {
     await promisify(this.client.del).bind(this.client)(key);
   }
+
+  async hset(hashKey, key, value) {
+    await promisify(this.client.hSet).bind(this.client)(hashKey, key, value);
+  }
+  async hgetAll(hashKey) {
+    const value = await promisify(this.client.hGetAll).bind(this.client)(hashKey);
+    return value;
+  }
 }
 
 const redisClient = new RedisClient();
