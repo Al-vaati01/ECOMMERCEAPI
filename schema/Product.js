2b7ff51fb7db476127b8f2f1ea8b1ebc3587f48c
Alvin <alvinvaati@gmail.com>
2023-10-17 22:35:47 +0300

    product schema

---
+++ schema/Product.js
@@ -0,0 +1,104 @@
+import dbClient from '../utils/db.js';
+
+const product = new dbClient.con.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        sku: {
+            type: String,
+            required: true
+        },
+        brand: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        subcategory: {
+            type: String
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        minStockLevel: {
+            type: Number
+        },
+        maxStockLevel: {
+            type: Number
+        },
+        reorderLevel: {
+            type: Number
+        },
+        images: {
+            type: [String]
+        },
+        videos: {
+            type: [String]
+        },
+        size: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        material: {
+            type: String
+        },
+        style: {
+            type: String
+        },
+        weight: {
+            type: Number
+        },
+        dimensions: {
+            type: String
+        },
+        shippingWeight: {
+            type: Number
+        },
+        shippingDimensions: {
+            type: String
+        },
+        shippingClass: {
+            type: String
+        },
+        shippingMethods: {
+            type: [String]
+        },
+        relatedProducts: {
+            type: [String]
+        },
+        reviews: {
+            type: [
+                {
+                    user: String,
+                    comment: String,
+                    rating: Number
+                }
+            ]
+        },
+        addedOn: {
+            type: Date,
+            default: Date.now()
+        },
+        updatedAt: {
+            type: Date.now()
+        }
+    }
+);
+const Product = dbClient.con.model('products', product);
+
+export default Product;
