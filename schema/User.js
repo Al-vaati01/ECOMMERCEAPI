1693b65c680368cb972e58486671baae04259b00
Alvin <alvinvaati@gmail.com>
2023-10-17 22:35:27 +0300

    users schema

---
+++ schema/User.js
@@ -0,0 +1,82 @@
+import dbClient from '../utils/db.js';
+
+const user = new dbClient.con.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const admin = new dbClient.con.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now()
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const User = dbClient.con.model('users', user);
+User.on('index', function (err) {
+    if (err) console.error(err);
+});
+const Admin = dbClient.con.model('admins', admin);
+
+export { User, Admin };
