d7f96b214e6a3350a657bcdf1959bfbba4c5f29e
Alvin <alvinvaati@gmail.com>
2023-10-18 16:24:49 +0300

    product controller

---
+++ controllers/ProductController.js
@@ -0,0 +1,23 @@
+class ProductController {
+    static getAllProducts(req, res) {
+        // implementation for getting all products
+    }
+
+    static getProductById(req, res) {
+        // implementation for getting a product by id
+    }
+
+    static createProduct(req, res) {
+        // implementation for creating a new product
+    }
+
+    static updateProduct(req, res) {
+        // implementation for updating a product by id
+    }
+
+    static deleteProduct(req, res) {
+        // implementation for deleting a product by id
+    }
+}
+
+export default ProductController;
