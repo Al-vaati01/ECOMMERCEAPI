f78fde0ce007b78d9633a8761dee31a52c32a254
Alvin <alvinvaati@gmail.com>
2023-10-18 16:26:07 +0300

    admin controller

---
+++ controllers/AdminController.js
@@ -0,0 +1,26 @@
+class AdminController {
+    static createProduct(req, res) {
+        // implementation
+    }
+
+    static updateProduct(req, res) {
+        // implementation
+    }
+
+    static deleteProduct(req, res) {
+        // implementation
+    }
+
+    static getAllOrders(req, res) {
+        // implementation
+    }
+
+    static getOrderById(req, res) {
+        // implementation
+    }
+
+    static updateOrderStatus(req, res) {
+        // implementation
+    }
+}
+export default AdminController;
