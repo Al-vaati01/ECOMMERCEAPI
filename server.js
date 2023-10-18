937ce20b2067e99f2773e59a16aaee68c138b56d
Alvin <alvinvaati@gmail.com>
2023-10-18 16:23:51 +0300

    server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
+import {sessionConfig} from './middleware/sessionConfig.js';
 
 // Load environment variables
 dotenv.config();
@@ -15,6 +16,10 @@ const app = express();
 app.use(cors());
 // Parse JSON
 app.use(express.json({limit:'200mb'}));
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+// Use session
+app.use(sessionConfig);
 
 // Routes
 app.use('/api/v1', router);
