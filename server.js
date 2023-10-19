350fa0cb16a048587ac44c2ad07949c1c7e5eaa7
Alvin <alvinvaati@gmail.com>
2023-10-19 19:07:34 +0300

    server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
-import {sessionConfig} from './middleware/sessionConfig.js';
+import { sessionConfig } from './middleware/sessionConfig.js';
+import cookieParser from 'cookie-parser';
 
 // Load environment variables
 dotenv.config();
@@ -11,11 +12,15 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    sess.cookie.secure = true // serve secure cookies
+}
+app.use(cookieParser());
 // Allow CORS
 app.use(cors());
 // Parse JSON
-app.use(express.json({limit:'200mb'}));
+app.use(express.json({ limit: '200mb' }));
 // Parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 // Use session
