fe785ead39674e5136d30d1c74d795e3492ffd79
Alvin <alvinvaati@gmail.com>
2024-01-13 20:17:16 +0300

    db util

--- utils/db.js
+++ utils/db.js
@@ -1,40 +1,42 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const host = process.env.DB_HOST || 'localhost';
-const port = process.env.DB_PORT || 27017;
-const database = process.env.DB_DATABASE || console.error('Database env variable not set');
-
 class DbClient {
-    constructor() {
-        const url = `mongodb://${host}:${port}/${database}`;
-        this.alive = this.connectDb(url);
-    }
-    connectDb(url) {
-        try {
-            mongoose.connect(url, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            });
-            if (mongoose.connection.readyState === 0) {
-                mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
-                return false;
-            }else {
-                console.log('Connected to DB');
-                this.con = mongoose;
-                return true;
-            }
-        } catch (error) {
-            console.error('Error connecting to DB', error);
-            return false
-        }
-    }
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || console.error('Database env variable not set');
 
-    isAlive() {
-        return this.alive;
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.alive = this.connectDb(url);
+  }
+
+  connectDb(url) {
+    try {
+      mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      if (mongoose.connection.readyState === 0) {
+        mongoose.connection.on('error', console.error.bind(console, 'Connection error:'));
+        return false;
+      } else {
+        console.log('Connected to MongoDB');
+        this.con = mongoose;
+        return true;
+      }
+    } catch (error) {
+      console.error('Error connecting to MongoDB', error);
+      return false;
     }
+  }
+
+  isAlive() {
+    return this.alive;
+  }
 }
+
 const dbClient = new DbClient();
 export default dbClient;
