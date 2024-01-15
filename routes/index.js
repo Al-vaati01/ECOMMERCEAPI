8e490f7c93f4ee999bd07a33bf5702d2086dfb1b
Alvin <alvinvaati@gmail.com>
2024-01-15 19:06:17 +0300

    .

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ import product from './product.js';
 
 const router = express.Router();
 
+// Welcome Page
+router.get('/', (req, res) => {
+    res.send(
+        {
+            status: 'OK',
+            message: 'Welcome e-com API'
+        }
+    ).status(200);
+}
+);
 router.use('/user', user);
 router.use('/admin', admin);
 router.use('/product', product);
