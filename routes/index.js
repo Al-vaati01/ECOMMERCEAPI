3da5cc9279a864a40b58c77bbdd2f33c9226d9de
Alvin <alvinvaati@gmail.com>
2024-01-15 20:05:22 +0300

    routes info

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,33 @@ router.get('/', (req, res) => {
     res.send(
         {
             status: 'OK',
-            message: 'Welcome e-com API'
+            message: 'Welcome e-com API',
+            rooutes:
+                {
+                    user: {
+                        login: '/api/v1/user/login',
+                        register: '/api/v1/user/register',
+                        logout: '/api/v1/user/logout',
+                        profile: '/api/v1/user/profile',
+                        update: '/api/v1/user/update',
+                        delete: '/api/v1/user/delete',
+                    },
+                    admin: {
+                        login: '/api/v1/admin/login',
+                        register: '/api/v1/admin/register',
+                        logout: '/api/v1/admin/logout',
+                        profile: '/api/v1/admin/profile',
+                        update: '/api/v1/admin/update',
+                        delete: '/api/v1/admin/delete',
+                    },
+                    product: {
+                        add: '/api/v1/product/add',
+                        update: '/api/v1/product/update',
+                        delete: '/api/v1/product/delete',
+                        get: '/api/v1/product/get',
+                        getall: '/api/v1/product/getall',
+                    }
+                }
         }
     ).status(200);
 }
