573ef315aa26ea0628d632eae4708fb45b21b79a
Alvin <alvinvaati@gmail.com>
2024-01-15 18:33:34 +0300

    Enable route not found error handling

--- server.js
+++ server.js
@@ -35,9 +35,12 @@ app.use((err, req, res, next) => {
 });
 
 //(Route not found)
-// app.use((req, res, next) => {
-//     res.status(404).json({ error: 'Route not Found' });
-// });
+app.use((req, res, next) => {
+    if (res.statusCode === 404) {
+        return res.status(404).json({ error: 'Not Found' });
+    }
+    next();
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
